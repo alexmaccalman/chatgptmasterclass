da3c6e89b25f76fd3786e5f889d5d936e2b6e443
alex.maccalman <alex.maccalman@gmail.com>
2023-05-28 21:53:27 -0400

    working function

--- basicazurefunction/completionapi/__init__.py
+++ basicazurefunction/completionapi/__init__.py
@@ -2,33 +2,48 @@ import logging
 import openai
 from dotenv import load_dotenv, find_dotenv
 import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import azure.functions as func
 
-_ = load_dotenv(find_dotenv())  # read local .env file
-secret_key = os.getenv("OPENAI_API_KEY")
-
 # sample request body
- # {"model":"text-davinci-003", "prompt":"Tell me a slogan for a home security company", "max_tokens": 200, "temperature": 0.0}
+# {"model": "davinci", "prompt": "give me a slogan for a cookie company", "max_tokens": 200, "temperature": 0}
+
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+# Azure Key Vault configuration
+vault_url = "https://resumekeyvaultadm.vault.azure.net/"
+secret_name = "OPENAI-API-KEY"
+
+# Create a DefaultAzureCredential object to authenticate with Azure Key Vault
+credential = DefaultAzureCredential()
+
+# Create a SecretClient to access the Azure Key Vault
+secret_client = SecretClient(vault_url=vault_url, credential=credential)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # give OpenAI our secret_key to authenticate
+    # Retrieve the OPENAI_API_KEY from Azure Key Vault
+    secret_key = secret_client.get_secret(secret_name).value
+
+    # Give OpenAI our secret_key to authenticate
     openai.api_key = secret_key
 
-    # get variables from the HTTP response
+    # Rest of your code...
     req_body = req.get_json()
-    #comment 2
-    # call the OpenAI API
+
+    # Call the OpenAI API
     output = openai.Completion.create(
         model=req_body['model'],
         prompt=req_body['prompt'],
         max_tokens=req_body['max_tokens'],
         temperature=req_body['temperature'],
     )
-    # format the response
+
+    # Format the response
     output_text = output['choices'][0]['text']
-    # echo the response
 
-
+    # Echo the response
     return func.HttpResponse(output_text, status_code=200)
